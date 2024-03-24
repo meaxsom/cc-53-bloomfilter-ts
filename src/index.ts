c538ed043829f24b682e295e89c2f5fea47a803a
Matthew E. Axsom <git@tipsydeveloper.io>
2024-03-24 19:37:06 +0000

    step 5.. test some actual words on the filter

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,38 @@ import Bloom  from './bloom';
 
 const kDefaultProbabilty    = 0.05;
 
+// configure log4js
 log4js.configure({
     appenders: { fileAppender: { type: "file", filename: "logs/bloom-filter.log" } },
     categories: { default: { appenders: ["fileAppender"], level: "debug" } },
   });
 
+// set up the command line
 const theCmdLine = new Command()
-    .option('-b, --build <string>', 'a file path to a dicionary file')
-    .option('-p, --prob <number>', 'the bloom percent probability number as a decimal. Default is ' + kDefaultProbabilty, String(kDefaultProbabilty))
-    .argument('[bloomfile]', 'path to the boloom file to be used. If used with build, will output to that file');
+    .option('-b, --build [string]', 'a file path to a dicionary file')
+    .option('-p, --prob [number]', 'the bloom percent probability number as a decimal. Used only wth "build"', String(kDefaultProbabilty))
+    .arguments('<bloomfile> [args...]');
 
 theCmdLine.parse(process.argv);
-let theProbability = Number(theCmdLine.getOptionValue('prob'))
 
-var theBloomFilter : Bloom;
+// process a build command or run the bloom filter on some supplied words
 if (theCmdLine.getOptionValue('build')) {
-    theBloomFilter = new Bloom(theCmdLine.getOptionValue('build'), theProbability);
+    let theProbability = Number(theCmdLine.getOptionValue('prob'))
+    let theBloomFilter = new Bloom(theCmdLine.getOptionValue('build'), theProbability);
     let theOutputFile = theCmdLine.args[0];
     if (theOutputFile)
         theBloomFilter.writeFilter(theOutputFile);
+} else {
+    let theArgs = theCmdLine.args;
+    if (theArgs.length > 0) {
+        let theBloomFilter = Bloom.readFilter(theArgs[0]);
+        if (theBloomFilter) {
+            console.log("These words are spelt wrong:");
+
+            for (var i=1; i<theArgs.length; i++) {
+                if (!theBloomFilter.isSet(theArgs[i]))
+                    console.log("\t" + theArgs[i]);
+            }
+        }
+    }
 }
